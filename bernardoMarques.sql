9c7bb2aec8075568b8bb7f61345943a9a41b2a4f
Bernardo Marques <sardinhaber@gmail.com>
2024-11-05 10:28:17 -0300

    feat: inserts hospedes

--- bernardoMarques.sql
+++ bernardoMarques.sql
@@ -26,11 +26,18 @@ CREATE TABLE reservas (
     id_reserva SERIAL PRIMARY KEY,
     id_hospede INT NOT NULL,
     id_quarto INT NOT NULL,
+    data_reserva DATE NOT NULL,
     data_inicio DATE NOT NULL,
-    data_fim DATE NOT NULL,
-    ativa VARCHAR(20) NOT NULL,
+    data_fim DATE,
     FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
     FOREIGN KEY (id_quarto) REFERENCES quartos (id_quarto)
 );
 
 SELECT * FROM reservas;
+
+INSERT INTO hospedes (nome, sexo, idade, email, telefone) VALUES
+('Bernardo', 'Masculino', 19, 'bernardo@gmail.com', '(19) 99367-1234'),
+('Maria', 'Feminino', 34, 'maria@gmail.com', '(19) 99234-9872'),
+('João', 'Masculino', 25, 'joao@gmail.com', '(19) 99723-9653'),
+('Eduardo', 'Masculino', 20, 'eduardo@gmail.com', '(19) 99731-1920');
+
