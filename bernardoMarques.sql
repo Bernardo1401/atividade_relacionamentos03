e57b060ad45427efd3f1508ba44a595e021d1465
Bernardo Marques <sardinhaber@gmail.com>
2024-11-05 09:17:22 -0300

    feat: crate table quartos

--- bernardoMarques.sql
+++ bernardoMarques.sql
@@ -9,6 +9,15 @@ CREATE TABLE hospedes (
     idade INT NOT NULL CHECK (IDADE > 18),
     email VARCHAR(100) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
-)
+);
 
 SELECT * FROM hospedes;
+
+CREATE TABLE quartos (
+    id_quarto SERIAL PRIMARY KEY,
+    numero INT NOT NULL,
+    suite BOOLEAN NOT NULL,
+    reservado BOOLEAN NOT NULL
+);
+
+SELECT * FROM quartos;
