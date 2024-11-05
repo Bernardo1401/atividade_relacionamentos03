8afa39eb9cca546317ab9d732c83f5beb9491f2e
Bernardo Marques <sardinhaber@gmail.com>
2024-11-05 09:41:44 -0300

    feat: create table reservas

--- bernardoMarques.sql
+++ bernardoMarques.sql
@@ -21,3 +21,16 @@ CREATE TABLE quartos (
 );
 
 SELECT * FROM quartos;
+
+CREATE TABLE reservas (
+    id_reserva SERIAL PRIMARY KEY,
+    id_hospede INT NOT NULL,
+    id_quarto INT NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    ativa VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+    FOREIGN KEY (id_quarto) REFERENCES quartos (id_quarto)
+);
+
+SELECT * FROM reservas;
